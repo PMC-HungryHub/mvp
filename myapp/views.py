8e6cb6337c27a704f532b9374b4a02eb2922e4de
Nicolás Murillo <111019351+nicolas23589@users.noreply.github.com>
2024-05-12 04:36:34 -0500

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,26 @@
+import pymongo
+import sys
+from django.shortcuts import redirect, render
+from .models import Plato
+
+
+client = pymongo.MongoClient ('mongodb://hungryhub84:fFgkg89qCuK1KJ3a@ac-t4pykdt-shard-00-00.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-01.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-02.pbi899i.mongodb.net:27017/?ssl=true&replicaSet=atlas-654uob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
+db = client.HungryHub
+platos = db["Platos"].find()
+
+
+def lista_platos(request):
+
+    return render(request, 'lista_platos.html', {'platos': platos})
+
+
+
+"""def pedir_plato(request, plato_id):
+    client = pymongo.MongoClient ('mongodb://hungryhub84:fFgkg89qCuK1KJ3a@ac-t4pykdt-shard-00-00.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-01.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-02.pbi899i.mongodb.net:27017/?ssl=true&replicaSet=atlas-654uob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
+    db = client.HungryHub
+    pedidos = db["Pedidos"].find()
+    pedidos.insert_one({"nombre": plato_nombre, "id": plato_id })
+
+
+    return HttpResponse("¡Plato pedido! ID del plato: {}".format(plato_id))
+"""
