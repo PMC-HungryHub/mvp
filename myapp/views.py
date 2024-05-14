b47a9dc9895ea810899b56f5f9cf196a4c0a93b9
Nicolás23589 <n.murilloc@uniandes.edu.co>
2024-05-14 16:43:52 -0500

    nuevo

--- myapp/views.py
+++ myapp/views.py
@@ -6,18 +6,17 @@ from .models import Plato
 
 client = pymongo.MongoClient ('mongodb://hungryhub84:fFgkg89qCuK1KJ3a@ac-t4pykdt-shard-00-00.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-01.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-02.pbi899i.mongodb.net:27017/?ssl=true&replicaSet=atlas-654uob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
 db = client.HungryHub
-platos = db["Platos"].find()
 
-print(platos)
+
 
 
 def lista_platos(request):
-
+    platos = db["Platos"].find()
     return render(request, 'lista_platos.html', {'platos': platos})
 
 
 
-"""def pedir_plato(request, plato_id):
+def pedir_plato(request, plato_id):
     client = pymongo.MongoClient ('mongodb://hungryhub84:fFgkg89qCuK1KJ3a@ac-t4pykdt-shard-00-00.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-01.pbi899i.mongodb.net:27017,ac-t4pykdt-shard-00-02.pbi899i.mongodb.net:27017/?ssl=true&replicaSet=atlas-654uob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
     db = client.HungryHub
     pedidos = db["Pedidos"].find()
