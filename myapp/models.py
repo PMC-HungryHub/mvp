ea57791e5aadd3073ef1a6973b30dfd866b08ae0
juanppjaime <juanppjame53@gmail.com>
2024-05-15 10:24:18 -0500

    Carrito de compra

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ class Plato(models.Model):
     id2= models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=0)
+    descripcion = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
